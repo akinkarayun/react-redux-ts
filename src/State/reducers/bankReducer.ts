9f985b6a0b7cd30f5e11920fdf28098bfb408091
Akin Karayun <akinkarayun1@hotmail.com>
2022-08-08 19:22:59 +0300

    created the reducer and specified the types for action

---
+++ src/State/reducers/bankReducer.ts
@@ -0,0 +1,21 @@
+import { ActionTypes } from "../action-types"
+import { Action } from "../actions"
+
+const initialState = 0
+
+const reducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case ActionTypes.DEPOSIT:
+      return state + action.payload
+
+    case ActionTypes.WITHDRAW:
+      return state - action.payload
+
+    case ActionTypes.BANKRUPT:
+      return 0
+    default:
+      return state
+  }
+}
+
+export default reducer
